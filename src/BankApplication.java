875e6a284ee58cad0c11f2166f265e22d4d70d31
Teodor Todorov <teditods13@gmail.com>
2022-07-07 04:02:47 +0300

    #1 - DataBase
    DataBase fully implemented!
    Users can now:
    -Create an account with a unique ID.
    -Deposit or withdraw funds.
    -Display all accounts that are saved into the DataBase.

--- src/BankApplication.java
+++ src/BankApplication.java
@@ -3,35 +3,27 @@ import java.util.Scanner;
 public class BankApplication {
     public static void main(String arg[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("How many customers do you want to create ?");
-             BankDetails cA;
-            cA = new BankDetails();
-            cA.openAccount();
-
+        BankDetails bd;
+        bd = new BankDetails();
         int ch;
         do {
             System.out.println("==================================================");
             System.out.println("=======================BANK=======================");
-            System.out.println("1. Display all registered accounts \n 2. Search By ID\n 3. Deposit the amount \n 4. Withdraw the amount \n 5. Exit");
+            System.out.println("1 Create Account \n 2. Display all registered accounts \n 3. Deposit money\n 4. Withdraw money \n 5. Exit");
             System.out.println("Input: ");
             ch = sc.nextInt();
             switch(ch) {
-
                 case 1:
-                    BankDetails sa;
-                    sa = new BankDetails();
-                    sa.showAccount();
+                    bd.openAccount();
                     break;
                 case 2:
-                    BankDetails dep;
-                    dep = new BankDetails();
-                    dep.deposit();
+                    bd.showAccount();
                     break;
                 case 3:
-                    System.out.print("Enter the account ID you searching for: ");
+                    bd.deposit();
                     break;
                 case 4:
-                    System.out.print("Enter the account ID you searching for: ");
+                    bd.withdraw();
                     break;
                 case 5:
                     System.out.println("Exiting application!");
--- src/BankDetails.java
+++ src/BankDetails.java
@@ -3,37 +3,26 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Scanner;
+import static java.lang.Runtime.getRuntime;
 
 public class BankDetails {
     String url="jdbc:mysql://localhost:3306/bankapp";
     String user="root";
     String pass="1234";
-    private double balance;
     Scanner sc = new Scanner(System.in);
+    //Creating account
     public void openAccount(){
         System.out.println("==================================================");
         System.out.println("=================Account Creation=================");
         try {
             Connection connection = DriverManager.getConnection(url, user, pass);
-
             Statement statement = connection.createStatement();
-
-            Scanner scanner=new Scanner(System.in);
             System.out.println("Enter Username");
-            String username=scanner.next();
+            String username=sc.next();
             System.out.println("Enter balance");
-            int balance=scanner.nextInt();
-
+            int balance=sc.nextInt();
             statement.executeUpdate("insert into users(name, balance) value('"+username+"','"+balance+"')");
             System.out.println("Values inserted successfully!");
-
-
-            /*ResultSet resultSet = statement.executeQuery("select * from users");
-
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString("name"));
-                System.out.println(resultSet.getString("balance"));
-            }*/
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -44,12 +33,8 @@ public class BankDetails {
         System.out.println("=================Account Details==================");
         try {
             Connection connection = DriverManager.getConnection(url, user, pass);
-
             Statement statement = connection.createStatement();
-
-
             ResultSet resultSet = statement.executeQuery("select * from users");
-
             while (resultSet.next()) {
                 System.out.println("==================================================");
                 System.out.print("User ID: " + resultSet.getString("id"));
@@ -66,42 +51,50 @@ public class BankDetails {
         double amount;
         System.out.println("==================================================");
         System.out.println("=================Account Deposit==================");
-        /*System.out.println("Enter the amount you want to deposit: ");
-        amount = sc.nextDouble();*/
         try {
             Connection connection = DriverManager.getConnection(url, user, pass);
-
             Statement statement = connection.createStatement();
-
-            String updateQuery="update users set name = 'Todek', balance='5000' where id= 1";
-            statement.executeUpdate(updateQuery);
+            ResultSet resultSet = statement.executeQuery("select * from users");
+            if(resultSet.next()){
+            System.out.println("Enter ID");
+            int userId=sc.nextInt();
+            System.out.println("Deposit amount: ");
+            int deposit=sc.nextInt();
+            int balance = resultSet.getInt("balance");
+            int newBalance = deposit + balance;
+                String updateQuery="update users set balance="+newBalance+" where id="+userId+"";
+                statement.executeUpdate(updateQuery);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
     }
-
-    /*public void withdraw(){
+    //Withdrawing funds
+    public void withdraw(){
         double amount;
         System.out.println("==================================================");
-        System.out.println("=================Account Withdraw=================");
-        System.out.println("Enter the amount to withdraw: ");
-        amount = sc.nextDouble();
-        if(balance >= amount){
-            balance = balance - amount;
-            System.out.println(("Balance after withdraw: " + balance));
+        System.out.println("=================Account Deposit==================");
+        try {
+            Connection connection = DriverManager.getConnection(url, user, pass);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from users");
+            if(resultSet.next()){
+                System.out.println("Enter ID");
+                int userId=sc.nextInt();
+                System.out.println("Withdraw amount: ");
+                int withdraw=sc.nextInt();
+                int balance = resultSet.getInt("balance");
+                int newBalance = balance - withdraw;
+                if(withdraw > balance)
+                {
+                    System.out.println("You don't have sufficient funds!");
+                    Runtime.getRuntime().exit(1);
+                }
+                String updateQuery="update users set balance="+newBalance+" where id="+userId+"";
+                statement.executeUpdate(updateQuery);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        else {
-        System.out.println("Your balance is less than " + amount + ".\tTransaction failed!");
-        }
-
     }
-    public boolean search(String ac_id){
-        if(accid.equals(ac_id)){
-            showAccount();
-            return(true);
-        }
-        return (false);
-    }*/
 }
