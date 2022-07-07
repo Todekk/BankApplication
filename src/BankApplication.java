6eed765003f2525b7bd82f357330a73d21b3571e
Teodor Todorov <teditods13@gmail.com>
2022-07-07 04:27:11 +0300

    Final touches.
    -Added a notification when you deposit or withdraw money.

--- src/BankApplication.java
+++ src/BankApplication.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class BankApplication {
-    public static void main(String arg[]){
+    public static void main(String arg[]) {
         Scanner sc = new Scanner(System.in);
         BankDetails bd;
         bd = new BankDetails();
@@ -12,7 +12,7 @@ public class BankApplication {
             System.out.println("1 Create Account \n 2. Display all registered accounts \n 3. Deposit money\n 4. Withdraw money \n 5. Exit");
             System.out.println("Input: ");
             ch = sc.nextInt();
-            switch(ch) {
+            switch (ch) {
                 case 1:
                     bd.openAccount();
                     break;
--- src/BankDetails.java
+++ src/BankDetails.java
@@ -4,31 +4,34 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.text.DecimalFormat;
 import java.util.Scanner;
+
 import static java.lang.Runtime.getRuntime;
 
 public class BankDetails {
-    String url="jdbc:mysql://localhost:3306/bankapp";
-    String user="root";
-    String pass="1234";
+    String url = "jdbc:mysql://localhost:3306/bankapp";
+    String user = "root";
+    String pass = "1234";
     Scanner sc = new Scanner(System.in);
+
     //Creating account
-    public void openAccount(){
+    public void openAccount() {
         System.out.println("==================================================");
         System.out.println("=================Account Creation=================");
         try {
             Connection connection = DriverManager.getConnection(url, user, pass);
             Statement statement = connection.createStatement();
             System.out.println("Enter Username");
-            String username=sc.next();
+            String username = sc.next();
             System.out.println("Enter balance");
-            int balance=sc.nextInt();
-            statement.executeUpdate("insert into users(name, balance) value('"+username+"','"+balance+"')");
+            int balance = sc.nextInt();
+            statement.executeUpdate("insert into users(name, balance) value('" + username + "','" + balance + "')");
             System.out.println("Values inserted successfully!");
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-//Displaying account details
+
+    //Displaying account details
     public void showAccount() {
         System.out.println("==================================================");
         System.out.println("=================Account Details==================");
@@ -42,39 +45,42 @@ public class BankDetails {
                 System.out.print("User ID: " + resultSet.getString("id"));
                 System.out.print("\n Username: " + resultSet.getString("name"));
                 System.out.print("\n Balance: " + resultSet.getString("balance"));
-                System.out.print("\n Balance (Euro): €" + f.format((resultSet.getDouble("balance")*0.51)));
-                System.out.print("\n Balance (Dollars): $" + f.format((resultSet.getDouble("balance")*0.53)));
-                System.out.print("\n Balance (Yen): ¥" + f.format((resultSet.getDouble("balance")*70.68)));
+                System.out.print("\n Balance (Euro): €" + f.format((resultSet.getDouble("balance") * 0.51)));
+                System.out.print("\n Balance (Dollars): $" + f.format((resultSet.getDouble("balance") * 0.53)));
+                System.out.print("\n Balance (Yen): ¥" + f.format((resultSet.getDouble("balance") * 70.68)));
                 System.out.println("\n==================================================");
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
     //Depositing funds
-    public void deposit(){
+    public void deposit() {
         System.out.println("==================================================");
         System.out.println("=================Account Deposit==================");
         try {
             Connection connection = DriverManager.getConnection(url, user, pass);
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("select * from users");
-            if(resultSet.next()){
-            System.out.println("Enter ID");
-            int userId=sc.nextInt();
-            System.out.println("Deposit amount: ");
-            double deposit=sc.nextInt();
-            double balance = resultSet.getInt("balance");
-            double newBalance = deposit + balance;
-                String updateQuery="update users set balance="+newBalance+" where id="+userId+"";
+            if (resultSet.next()) {
+                System.out.println("Enter ID");
+                int userId = sc.nextInt();
+                System.out.println("Deposit amount: ");
+                double deposit = sc.nextInt();
+                double balance = resultSet.getInt("balance");
+                double newBalance = deposit + balance;
+                String updateQuery = "update users set balance=" + newBalance + " where id=" + userId + "";
                 statement.executeUpdate(updateQuery);
+                System.out.println("Money successfully deposited!");
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
     //Withdrawing funds
-    public void withdraw(){
+    public void withdraw() {
         double amount;
         System.out.println("==================================================");
         System.out.println("=================Account Withdraw=================");
@@ -82,20 +88,20 @@ public class BankDetails {
             Connection connection = DriverManager.getConnection(url, user, pass);
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("select * from users");
-            if(resultSet.next()){
+            if (resultSet.next()) {
                 System.out.println("Enter ID");
-                int userId=sc.nextInt();
+                int userId = sc.nextInt();
                 System.out.println("Withdraw amount: ");
-                double withdraw=sc.nextInt();
+                double withdraw = sc.nextInt();
                 double balance = resultSet.getInt("balance");
                 double newBalance = balance - withdraw;
-                if(withdraw > balance)
-                {
+                if (withdraw > balance) {
                     System.out.println("You don't have sufficient funds!");
                     Runtime.getRuntime().exit(1);
                 }
-                String updateQuery="update users set balance="+newBalance+" where id="+userId+"";
+                String updateQuery = "update users set balance=" + newBalance + " where id=" + userId + "";
                 statement.executeUpdate(updateQuery);
+                System.out.println("Money successfully withdrawn!");
             }
         } catch (Exception e) {
             e.printStackTrace();
